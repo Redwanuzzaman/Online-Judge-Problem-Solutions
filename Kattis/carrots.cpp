f018d384c49170eef33122b14225c4d01ef79684
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-01-20 17:49:14 +0600

    Solving for Carrots

---
+++ Kattis/carrots.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,p;
+    cin>>n>>p;
+    string s;
+    while(n--)
+    {
+        getchar();
+        getline(cin,s);
+    }
+    cout<<p<<endl;
+    return 0;
+}
