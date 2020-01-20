f3224d4ed01e1609d702f07e78422db7b613418b
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-01-20 17:14:34 +0600

    Timus 2001

---
+++ Timus/2001.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a1, a2, a3, b1, b2, b3;
+    cin >> a1 >> b1 >> a2 >> b2 >> a3 >> b3;
+    cout <<abs(a1-a3) << " " << abs(b1-b2) << endl;
+}
