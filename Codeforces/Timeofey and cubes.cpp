1d5ca40f7fa797a2615898c7af7f5ce4a60b4b14
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-02-06 00:42:56 +0600

    Create Timeofey and cubes.cpp

---
+++ Codeforces/Timeofey and cubes.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n+1];
+
+    for(int i = 1; i <= n; i++) cin >> arr[i];
+
+    for(int i = 1, j = n; i <= n/2; i+=2, j-=2) swap(arr[i], arr[j]);
+
+    for(int i = 1; i <= n; i++) cout << arr[i] << " ";
+}
