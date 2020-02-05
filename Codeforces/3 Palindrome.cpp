ab5146e6a55c0055d2624845746e353528ff677e
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-02-06 00:43:27 +0600

    Create 3 Palindrome.cpp

---
+++ Codeforces/3 Palindrome.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    string s;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        if(i % 4 < 2) cout << 'a';
+        else cout << 'b';
+    }
+}
+
+
