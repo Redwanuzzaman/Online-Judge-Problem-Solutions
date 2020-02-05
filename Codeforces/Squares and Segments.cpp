94da9d7894eb4700b936076cf01ca162873f8752
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-02-05 23:40:48 +0600

    Create Squares and Segments.cpp

---
+++ Codeforces/Squares and Segments.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    double sq = sqrt(n);
+    double res = (sq * 2.0);
+    cout << ceil(res * 1.0) << endl;
+}
