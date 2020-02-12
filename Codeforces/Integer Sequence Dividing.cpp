1403b02a1ae3540e8f7582b2b8c5a27e28e594d4
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-02-12 23:26:52 +0600

    Create Integer Sequence Dividing.cpp

---
+++ Codeforces/Integer Sequence Dividing.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define sft scanf("%lld",&t)
+#define sfn scanf("%lld",&n)
+#define sfl2 scanf("%lld%lld",&n,&m)
+#define sfarr scanf("%lld",&arr[i])
+#define pi acos(-1)
+
+int main()
+{
+    ll n;
+    sfn;
+    if(n % 4 == 0 || n % 4 == 3) cout << "0" << endl;
+    else cout << "1" << endl;
+}
