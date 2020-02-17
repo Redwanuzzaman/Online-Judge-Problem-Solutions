6c37e7813f04c951cb9e58f109eec5b6547b775c
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-02-18 00:55:50 +0600

    Create Odd and Even Count.cpp

---
+++ GYM/Odd and Even Count.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, s, d;
+    cin >> t;
+    while(t--)
+    {
+        cin >> s;
+        d = s/2;
+        cout << s-d << " " << d << endl;
+    }
+}
