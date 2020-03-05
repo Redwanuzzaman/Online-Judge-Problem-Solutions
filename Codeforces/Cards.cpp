80e7aee2c1bd567df1da25a42b326d7181ca7ee4
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-03-06 00:13:59 +0600

    Create Cards.cpp

---
+++ Codeforces/Cards.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    pair <int, int> p[n+1];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> p[i].first;
+        p[i].second = i + 1;
+    }
+    sort(p, p+n);
+
+    for(int i = 0; i < n/2; i++)
+        cout << p[i].second << " " << p[n-i-1].second << endl;
+}
