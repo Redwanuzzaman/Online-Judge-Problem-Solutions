b57d41f34d0fb79d8c9bb1242152b27e88f097aa
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-03-15 03:11:47 +0600

    Create EhAb AnD gCd.cpp

---
+++ Codeforces/EhAb AnD gCd.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int test, x;
+    cin >> test;
+    while(test--)
+    {
+        cin >> x;
+        cout << 1 << " " << x-1 << endl;
+    }
+}
