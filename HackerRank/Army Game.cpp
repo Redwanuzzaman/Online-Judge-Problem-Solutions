373a8d672d81adb5961de6150c03ee131a176f6a
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-03-20 02:56:38 +0600

    Create Army Game.cpp

---
+++ HackerRank/Army Game.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, m, ans;
+    cin >> n >> m;
+    
+    ans = (ceil(n/2.0)) * (ceil(m/2.0));
+    cout << ans << endl;
+}
