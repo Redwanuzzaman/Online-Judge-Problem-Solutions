6d9034a0f7f9a92b8c791452c5feab6e9384470c
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-03-27 23:38:54 +0600

    Create You Can Go Your Own Way.cpp

---
+++ Google Codejam/You Can Go Your Own Way.cpp
@@ -0,0 +1,26 @@
+// Google Codejam 2019, Round 0, Problem B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sft scanf("%d",&t)
+#define sfn scanf("%d",&n)
+
+int main()
+{
+    int t, n;
+    sft;
+    for(int i = 1; i <= t; i++)
+    {
+        sfn;
+        string s;
+        cin >> s;
+        printf("Case #%d: ", i);
+        for(int j = 0; j < s.size(); j++)
+        {
+            if(s[j] == 'E') cout << 'S';
+            else cout << 'E';
+        }
+        cout << endl;
+    }
+}
