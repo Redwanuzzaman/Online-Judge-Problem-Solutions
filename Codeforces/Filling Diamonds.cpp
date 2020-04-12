dab65fe2a8404cd1ee70d513b2787a7073df77b5
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-04-13 03:08:04 +0600

    Create Filling Diamonds.cpp

---
+++ Codeforces/Filling Diamonds.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sft scanf("%d",&t)
+#define sfn scanf("%d",&n)
+
+int main()
+{
+    int t, n;
+    sft;
+    while(t--)
+    {
+        sfn;
+        cout << n << endl;
+    }
+}
