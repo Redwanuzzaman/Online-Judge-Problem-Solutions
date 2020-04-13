c2bef79bbd9ef19f2b20557f9077fbfe9c386657
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-04-14 03:40:59 +0600

    Create Candies and Two Sisters.cpp

---
+++ Codeforces/Candies and Two Sisters.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sft scanf("%d",&t)
+#define sfn scanf("%d",&n)
+#define sfxy scanf("%d%d",&x,&y)
+#define sfnm scanf("%d%d",&n,&m)
+#define sfarr scanf("%d",&arr[i])
+#define sftll scanf("%lld",&t)
+#define sfnll scanf("%lld",&n)
+#define sfnmll scanf("%lld%lld",&n,&m)
+#define sfxyll scanf("%lld%lld",&x,&y)
+#define sfarrll scanf("%lld",&arr[i])
+#define sf2d scanf("%d%d",&arr[i][j])
+#define pi acos(-1)
+#define MAX 100000000
+typedef long long ll;
+
+int main()
+{
+    ll t, n, m;
+    sftll;
+    while(t--)
+    {
+        sfnll;
+        if(n < 3) cout << 0 << endl;
+        else
+        {
+            ll ans = ceil(n/2.0) - 1;
+            cout << ans << endl;
+        }
+    }
+}
