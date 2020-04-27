7f3393259ed9dc58f7cf91ec21b30cbe5d0779af
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-04-27 17:18:49 +0600

    Create Zipped.py

---
+++ HackerRank/Zipped.py
@@ -0,0 +1,14 @@
+def main():
+    n, x = input().split()
+    n = int(n)  # student count
+    x = int(x)  # subject count
+    grades = [list(map(float, input().split())) for i in range(x)]
+    grades_by_student = zip(*grades)
+    average_by_student = [sum(i) / x for i in list(grades_by_student)]
+    average_by_student = [round(i, 1) for i in average_by_student]
+
+    for i in average_by_student:
+        print(i)
+
+if __name__ == '__main__':
+    main()
