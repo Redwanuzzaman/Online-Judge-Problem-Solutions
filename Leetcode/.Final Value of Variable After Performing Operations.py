6b2a03f07c57a05506edd8a89a1f6041ff46e183
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2022-02-06 23:31:10 +0600

    Create .Final Value of Variable After Performing Operations.py

---
+++ Leetcode/.Final Value of Variable After Performing Operations.py
@@ -0,0 +1,9 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        result = 0
+        for op in operations:
+            if op in ["X++", "++X"]:
+                result += 1
+            else:
+                result -= 1
+        return result
