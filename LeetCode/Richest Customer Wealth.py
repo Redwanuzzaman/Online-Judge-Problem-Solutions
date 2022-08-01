6e2328a84add088e8408cdec467f294581c01969
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2022-08-02 00:56:41 +0600

    Delete Richest Customer Wealth.py

--- LeetCode/Richest Customer Wealth.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        max_money = 0
-        for banks in accounts:
-            current_money = sum(banks)
-            max_money = max(max_money, current_money)
-        return max_money
-                
